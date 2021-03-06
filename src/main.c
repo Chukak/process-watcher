290b6cdec41d42273f80f46bac455eb668b3b2f9
Sergey Fedorov <chukak12@gmail.com>
2021-03-06 19:24:40 +0300

    Run watching to the specified process
    Signals handling
    Clearing resources before exiting

--- src/main.c
+++ src/main.c
@@ -1,9 +1,54 @@
 #include "system-watcher-globals.h"
+#include "window.h"
+#include "process.h"
+#include "cmdargs.h"
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+static _Atomic bool Is_running = false;
+__attribute__((nothrow)) static void sighandler(int sig)
+{
+  UNUSED(sig);
+  Is_running = false;
+}
 
 __attribute__((nothrow)) int main(int argc, char** argv)
 {
   UNUSED(argc);
   UNUSED(argv);
+
+  signal(SIGINT, sighandler);
+  signal(SIGTERM, sighandler);
+
+  Cmd_args* args = Cmd_args_init(argc, argv);
+  if (args->Valid) {
+    Process_stat* stat = NULL;
+    Process_stat_init(&stat);
+
+    char* errormsg = NULL;
+    if (Process_stat_set_pid(&stat, args->Process_name, &errormsg)) {
+      Is_running = true;
+
+      Window* mainwin = Window_init();
+      while (Is_running) {
+        Window_refresh(mainwin, stat);
+        usleep(args->Refresh_timeout_ms * 1000); // usleep takes microseconds
+      }
+
+      Is_running = false;
+      Window_destroy(mainwin);
+    } else {
+      printf("%s\n", errormsg);
+    }
+
+    free(errormsg);
+    Process_stat_free(&stat);
+  } else {
+    printf("%s\n", args->Errormsg);
+  }
+
+  Cmd_args_free(args);
 
   return 0;
 }
